ff2bae91341a0e7dbf6548bf0ca5bba845f7f738
Lwamulungisheillah <143074288+Lwamulungisheillah@users.noreply.github.com>
2023-09-14 16:38:06 +0300

    LISTS/ COLLECTIONS

---
+++ lwamulungiassignment2.py
@@ -0,0 +1,29 @@
+#assignment_2
+#given_list_is_as_follows;
+students_marks = [60,80,90,98]
+print(students_marks)
+
+#(i) change 80 t0 89
+students_marks[1] = 89
+print(students_marks)
+
+#append item 55
+students_marks.append(55)
+print(students_marks)
+
+#size of the list having appended item 55
+print(len(students_marks))
+
+#program to sum all items in the list
+total = sum(students_marks)
+print(total)
+
+#function that takes two lists and returns true, if they have atleast one common member
+list_one= input("enter members")
+list_two= input("enter members")
+common_member= 5
+if 5 in list_one and list_two:
+     print("true")
+     
+
+    
